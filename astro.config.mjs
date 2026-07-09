f37b195b3aa33e48a9e2a9ca9287de3d55300873
Andrew <andrew.a@axiompro.biz>
2026-07-09 19:27:32 +0200

    Update content, block photos and set production domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://arsbudinvest.ua',
+  site: 'https://arsbudinvest.com',
   build: {
     inlineStylesheets: 'auto',
   },
